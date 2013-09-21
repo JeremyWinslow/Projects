318a2d8122886b025b8dbd7e4e605224155bfd14
Jeremy Winslow <JerBear@Lugia.local>
2013-09-20 20:43:15 -0700

    Initial Commit

---
+++ Blah/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blah
+//
+//  Created by Jeremy Winslow on 9/20/13.
+//  Copyright (c) 2013 Jeremy Winslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blah/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Blah
+//
+//  Created by Jeremy Winslow on 9/20/13.
+//  Copyright (c) 2013 Jeremy Winslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
